2700c2398f23a64c42045514bdfa05050fd95730
John S <tinco7@aol.com>
2020-08-18 00:06:34 -0700

    added market balance

--- assets/js/etf.js
+++ assets/js/etf.js
@@ -1,4 +1,7 @@
 var num = 1;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 function addRow(){
   $(".btn").mouseup(function(){
     $(this).blur();
@@ -65,6 +68,10 @@ function autoBalance(){
   $(".btn").mouseup(function(){
     $(this).blur();
   })
+  if(document.getElementById("availCapital").value == ""){
+    document.getElementById("availCapital").value = "WHY IS THIS BLANK?!?!"
+    return
+  }
     var table = document.getElementById("stonks")
     var rows = table.childElementCount
     var availCap = document.getElementById("availCapital").value
@@ -73,4 +80,60 @@ function autoBalance(){
         console.log(symbol)
         getQuote(symbol, i, availCap, table)
     }
+}
+async function getMarketCap(symbol, i, rows, marketCaps){
+  console.log(i)
+  await sleep(500)
+  var settings = {
+    "url": "https://finnhub.io/api/v1/stock/profile2?token=bsnpc1nrh5rctp1flcd0&symbol=" + symbol,
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "Cookie": "__cfduid=d404aa8f0146ae8a0c5ecc746e85c294d1597517827"
+    },
+  };
+  $.ajax(settings).done(async function (response) {
+    marketCaps[i].cap = response.marketCapitalization
+    if (marketCaps.length == rows){
+      calcWeights(marketCaps)
+    }
+    await sleep(500)
+    });
+}
+function marketBalance(){
+  $(".btn").mouseup(function(){
+    $(this).blur();
+  })
+  if(document.getElementById("availCapital").value == ""){
+    document.getElementById("availCapital").value = "WHY IS THIS BLANK?!?!"
+    return
+  }
+  var table = document.getElementById("stonks")
+  var rows = table.childElementCount
+  var marketCaps = []
+  for (let i = 0; i < rows; i++) {
+    marketCaps.push({i:i,cap:0})
+    var symbol = document.getElementById("symbol"+i).value.toUpperCase()
+    getMarketCap(symbol, i, rows, marketCaps);
+  }
+}
+function calcWeights(marketCaps){
+  var table = document.getElementById("stonks")
+  var rows = table.childElementCount
+  var totalCap = 0
+  for(i = 0; i < rows; i++){
+    totalCap += marketCaps[i].cap
+  }
+  for(i = 0; i < rows; i++){
+    document.getElementById("alloc" + i).value = ((marketCaps[i].cap/totalCap)*100)
+    console.log(((marketCaps[i].cap/totalCap)*100))
+  }
+  
+  for(i = 0; i < rows; i++){
+    var symbol = document.getElementById("symbol"+i).value.toUpperCase()
+    var table = document.getElementById("stonks")
+    var availCap = document.getElementById("availCapital").value
+    console.log(symbol)
+    getQuote(symbol, i, availCap, table)
+  }
 }
